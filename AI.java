49e2ffb34bfec5e9e43ce98d8e18d1dcbb2f0edf
leonardodistasio <leonardo.distasio@he-arc.ch>
2015-10-23 15:55:04 +0200

    Fonction evaluation à terminer

--- AI.java
+++ AI.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import Othello.Move;
 
 /**
- * Evaluation des cases (chances) :
+ * Evaluation des cases (chances) et référence de stratégie :
+ * 
  * http://imagine.enpc.fr/~monasse/Info/Projets/2003/othello.pdf
+ * http://emmanuel.adam.free.fr/site/IMG/pdf/jeuP.pdf
  *
  */
 
@@ -14,6 +16,7 @@ public class AI {
 
 	private int ennemyID;
 	private int playerID;
+	private boolean iBegin = false;
 	
 	private int[][] evaluation = new int[][]{
 			{500, -150, 30, 10, 10, 30, -150, 500},
@@ -26,7 +29,8 @@ public class AI {
 			{500, -150, 30, 10, 10, 30, -150, 500}
 	};
 
-	public AI() {
+	public AI() 
+	{
 	}
 
 	public Move getBestMove(GameBoard gameBoard, int depth, int playerID) 
@@ -34,13 +38,14 @@ public class AI {
 		this.ennemyID = 1- playerID;
 		this.playerID = playerID;
 		
-		GameBoard gameBoard2 = gameBoard.clone();
-		Move moveResult = null;
+		Node moveResult = new Node(null);
 		
-		for(Move m : gameBoard2.getPossibleMoves(playerID))
+		moveResult = ab_max(gameBoard, new Node(null), depth, Integer.MAX_VALUE);
+
+		/*for(Move m : gameBoard2.getPossibleMoves(playerID))
 		{
 			Node root = new Node(m);
-			Node result = ab_min(gameBoard2, root, depth, root.getEvaluation());
+			Node result = ab_max(gameBoard2, root, depth, root.getEvaluation());
 			
 			if(moveResult == null) moveResult = result.getMove();
 			else
@@ -50,24 +55,23 @@ public class AI {
 					moveResult = result.getMove();
 				}
 			}
-		}
+		}*/
 		
-		return moveResult;
+		return moveResult.getMove();
 	}
 
-	private Node ab_max(GameBoard gameBoard, Node root, int depth, double parentMin) {
+	private Node ab_max(GameBoard gameBoard, Node root, int depth, int parentMin) {
 
 		ArrayList<Move> listMoves = gameBoard.getPossibleMoves(playerID);
 		
-		if (root.isLeaf() || depth == 0)
+		if (depth == 0 || listMoves.isEmpty())
 		{
-			root.setEvaluation(getEvaluationMove(root.getMove()));
+			root.setEvaluation(getEvaluationMove(gameBoard));
 			return root;
 		}
 		
-		//double maxValue = Double.NEGATIVE_INFINITY;
 		Node maxNode = new Node(null);
-		maxNode.setEvaluation((int)Double.NEGATIVE_INFINITY);
+		int max = -Integer.MAX_VALUE;
 
 		for(Move m : listMoves)
 		{
@@ -75,54 +79,107 @@ public class AI {
 			newGameBoard.addCoin(m, playerID);
 			
 			Node newNode = new Node(m);
+			root.addChildNode(newNode);
 			
-			Node resultNode = ab_min(newGameBoard, newNode, depth-1, maxNode.getEvaluation());
+			Node resultNode = ab_min(newGameBoard, newNode, depth-1, max);
 			
-			if(resultNode.getEvaluation() > maxNode.getEvaluation())
+			if(resultNode.getEvaluation() > max)
 			{
-				maxNode = resultNode;
-				if(maxNode.getEvaluation() > parentMin) break;
+				max = resultNode.getEvaluation();
+				maxNode = newNode;
+				maxNode.setEvaluation(max);
+				
+				if(max > parentMin) break;
 			}
 		}
 		
 		return maxNode;
 	}
 	
-	private Node ab_min(GameBoard gameBoard, Node root, int depth, double parentMin) 
+	private Node ab_min(GameBoard gameBoard, Node root, int depth, int parentMax) 
 	{
 		ArrayList<Move> listMoves = gameBoard.getPossibleMoves(ennemyID);
 		
-		if (root.isLeaf() || depth == 0)
+		if (depth == 0 || listMoves.isEmpty())
 		{
-			root.setEvaluation(getEvaluationMove(root.getMove()));
+			root.setEvaluation(getEvaluationMove(gameBoard));
 			return root;
 		}
 		
-		//double minValue = Double.POSITIVE_INFINITY;
 		Node minNode = new Node(null);
-		minNode.setEvaluation((int)Double.NEGATIVE_INFINITY);
+		int min = Integer.MAX_VALUE;
 
 		for(Move m : listMoves)
 		{
 			GameBoard newGameBoard = gameBoard.clone();
-			newGameBoard.addCoin(m, playerID);
+			newGameBoard.addCoin(m, ennemyID);
 			
 			Node newNode = new Node(m);
+			root.addChildNode(newNode);
 			
-			Node resultNode = ab_max(newGameBoard, newNode, depth-1, minNode.getEvaluation());
+			Node resultNode = ab_max(newGameBoard, newNode, depth-1, min);
 			
-			if(resultNode.getEvaluation() > minNode.getEvaluation())
+			if(resultNode.getEvaluation() < min)
 			{
-				minNode = resultNode;
-				if(minNode.getEvaluation() > parentMin) break;
+				min = resultNode.getEvaluation();
+				minNode = newNode;
+				minNode.setEvaluation(min);
+				
+				if(min < parentMax) break;
 			}
 		}
-		
+
 		return minNode;
 	}
 	
-	private int getEvaluationMove(Move move)
+	private int getEvaluationMove(GameBoard game)
 	{
-		return evaluation[move.i][move.j]; 
+		int result = 0;
+		
+		int nbCaseMilieu = GameBoard.BOARD_SIZE * GameBoard.BOARD_SIZE;
+		int force = getForceBoard(game);
+		int materiel = game.getCoinCount(playerID);
+		int mobility = game.getPossibleMoves(playerID).size();
+		
+		int nbPionCoin = game.getCornerCoinCount(playerID);
+		int nbPionEdge = game.getEdgeCoinCount(playerID);
+		
+		//Ouverture
+		if((materiel + game.getCoinCount(ennemyID) - 4) <= 12)
+		{
+			//Mobilité et position favorisées.
+			//pPosition important et pMobilité au max
+			result = 10 * mobility + force;
+		}
+		//Fin
+		else if(game.getCoinCount(playerID) + game.getCoinCount(ennemyID) > nbCaseMilieu)
+		{
+			//pMatériel max (nombre jeton important)
+			result = nbPionEdge * 2 + materiel;
+		}
+		//Milieu
+		else
+		{
+			//Important bord et coins, pPosition max
+			result = 10 * force - 4 * nbPionCoin;
+		}
+		return result;
+	}
+
+	private int getForceBoard(GameBoard game)
+	{
+		int result = 0;
+		
+		for (int i = 0; i < game.BOARD_SIZE; i++) 
+		{
+			for (int j = 0; j < game.BOARD_SIZE; j++)
+			{
+				if(playerID == game.getPlayerIDAtPos(i, j))
+				{
+					result += evaluation[i][j];
+				}
+			}
+		}
+		return result;
 	}
 }
