dd2841c41cc6d245766a28d04381d906490d64a5
leonardodistasio <leonardo.distasio@he-arc.ch>
2015-10-27 08:58:17 +0100

    AI

--- AI.java
+++ AI.java
@@ -16,8 +16,9 @@ public class AI {
 
 	private int ennemyID;
 	private int playerID;
-	private boolean iBegin = false;
+	private int iBegin = 0;
 
+	//Tableau d'évaluation
 	private int[][] evaluation = new int[][] {
 			{ 500, -150, 30, 10, 10, 30, -150, 500 },
 			{ -150, -250, 0, 0, 0, 0, -250, -150 },
@@ -26,30 +27,25 @@ public class AI {
 			{ -150, -250, 0, 0, 0, 0, -250, -150 },
 			{ 500, -150, 30, 10, 10, 30, -150, 500 } };
 
-	public AI() {
+	public AI() 
+	{
 	}
 
+	//Fonction pour trouver le meilleure mouvement
 	public Move getBestMove(GameBoard gameBoard, int depth, int playerID) {
 		this.ennemyID = 1 - playerID;
 		this.playerID = playerID;
+		this.iBegin = (gameBoard.getCoinCount(playerID) + gameBoard.getCoinCount(ennemyID)) % 2;	//Savoir si on commence ou non
 
 		Node moveResult = new Node(null);
 
-		moveResult = ab_max(gameBoard, new Node(null), depth, Integer.MAX_VALUE);
-
-		/*
-		 * for(Move m : gameBoard2.getPossibleMoves(playerID)) { Node root = new
-		 * Node(m); Node result = ab_max(gameBoard2, root, depth,
-		 * root.getEvaluation());
-		 * 
-		 * if(moveResult == null) moveResult = result.getMove(); else {
-		 * if(getEvaluationMove(moveResult) < result.getEvaluation()) {
-		 * moveResult = result.getMove(); } } }
-		 */
+		changeValueCoinCornerOccuped(gameBoard);
+		moveResult = ab_max(gameBoard, new Node(null), depth, Integer.MAX_VALUE);	//Débuter l'alpha-beta
 
 		return moveResult.getMove();
 	}
 
+	//Fonction alpha-beta maximum
 	private Node ab_max(GameBoard gameBoard, Node root, int depth, int parentMin) {
 
 		ArrayList<Move> listMoves = gameBoard.getPossibleMoves(playerID);
@@ -84,6 +80,7 @@ public class AI {
 		return maxNode;
 	}
 
+	//Fonction alpha-beta minimum
 	private Node ab_min(GameBoard gameBoard, Node root, int depth, int parentMax) {
 		ArrayList<Move> listMoves = gameBoard.getPossibleMoves(ennemyID);
 
@@ -117,6 +114,8 @@ public class AI {
 		return minNode;
 	}
 
+	// Fonction d'évaluation pour les mouvements
+	// Stratégie différente si on commence ou non
 	private int getEvaluationMove(GameBoard game) {
 		int result = 0;
 
@@ -135,16 +134,25 @@ public class AI {
 		// Ouverture (-30 pions)
 		if (nbPionTotal < 30) {
 			// Mobilité et position favorisées.
-			result = 50 * mobility + force + 2 * materiel;
+			if(iBegin == 0)
+				result = 50 * mobility + force + 2 * materiel;
+			else
+				result = 30 * mobility + force + 10 * materiel;
 		}
 		// Fin (+ 45 pions)
 		else if (nbPionTotal > 45) {
-			result = force / 30 + materiel;
+			if(iBegin == 0)
+				result = force / 30 + materiel;
+			else
+				result = force / 20 + materiel;
 
 		}
 		// Milieu (entre 15 et 45 pions)
 		else {
-			result = 50 * mobility + force + 10 * materiel;
+			if(iBegin == 0)
+				result = 50 * mobility + force + 10 * materiel;
+			else
+				result = 30 * mobility + force + 4 * materiel;
 		}
 
 		return result;
@@ -153,13 +161,52 @@ public class AI {
 	private int getForceBoard(GameBoard game) {
 		int result = 0;
 
-		for (int i = 0; i < game.BOARD_SIZE; i++) {
-			for (int j = 0; j < game.BOARD_SIZE; j++) {
+		for (int i = 0; i < GameBoard.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameBoard.BOARD_SIZE; j++) {
 				if (playerID == game.getPlayerIDAtPos(i, j)) {
 					result += evaluation[i][j];
 				}
 			}
 		}
 		return result;
+	}
+	
+	private void changeValueCoinCornerOccuped(GameBoard game)
+	{		
+		if(playerID == game.getPlayerIDAtPos(0, 0))
+		{
+			changEvaluationTab(0, 0);
+		}
+		
+		if(playerID == game.getPlayerIDAtPos(0, GameBoard.BOARD_SIZE-1))
+		{
+			changEvaluationTab(0, GameBoard.BOARD_SIZE-1);
+		}
+		
+		if(playerID == game.getPlayerIDAtPos(GameBoard.BOARD_SIZE-1, 0))
+		{
+			changEvaluationTab(GameBoard.BOARD_SIZE-1, 0);
+		}
+		
+		if(playerID == game.getPlayerIDAtPos(GameBoard.BOARD_SIZE-1, GameBoard.BOARD_SIZE-1))
+		{
+			changEvaluationTab(GameBoard.BOARD_SIZE-1, GameBoard.BOARD_SIZE-1);
+		}
+	}
+	
+	private void changEvaluationTab(int x, int y)
+	{
+		int directionX = (x == GameBoard.BOARD_SIZE-1) ? -1 : 1;
+		int directionY = (y == GameBoard.BOARD_SIZE-1) ? -1 : 1;
+		
+		evaluation[x+directionX][y] = 150;
+		evaluation[x][y+directionY] = 150;
+		evaluation[x+directionX][y+directionY] = 250;
+		
+		for(int i = 0; i < GameBoard.BOARD_SIZE; i++)
+		{
+			evaluation[x][i] *= 3;
+			evaluation[i][y] *= 3;
+		}
 	}
 }
